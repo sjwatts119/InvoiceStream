2741a4279bebd2a19dabd8a7b30e3b8303da5465
Sam Watts <samuel.watts67@gmail.com>
2025-01-31 20:41:54 +0000

    Replace breeze components with flux

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,18 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/livewire/flux-pro/stubs/**/*.blade.php',
+        './vendor/livewire/flux/stubs/**/*.blade.php',
     ],
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                accent: {
+                    DEFAULT: 'var(--color-accent)',
+                    content: 'var(--color-accent-content)',
+                    foreground: 'var(--color-accent-foreground)',
+                },
             },
         },
     },
